7e6003c3fbfcf6cc37e3d94a4a4577401ce57e7a
Vaciuum <137624352+Vaciuum@users.noreply.github.com>
2024-12-03 20:01:54 +0400

    Delete website directory

--- website/script.js
+++
@@ -1,4 +0,0 @@
-// Simple JavaScript for any future interaction or animation (currently not needed)
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website is under construction...");
-});
