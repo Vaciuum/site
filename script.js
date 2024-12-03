24e1d23e725844a46ba57d512b11f8151d138e1b
Vaciuum <137624352+Vaciuum@users.noreply.github.com>
2024-12-03 20:02:19 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JavaScript for any future interaction or animation (currently not needed)
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website is under construction...");
+});
